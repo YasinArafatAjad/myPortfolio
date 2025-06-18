7e0515e72d54223b6499f87d6c0a50ed102f5a56
Yasin Arafat Ajad <136703649+YasinArafatAjad@users.noreply.github.com>
2025-06-19 05:01:16 +0600

    Updated ProjectCard.jsx

--- src/components/portfolio/ProjectCard.jsx
+++ src/components/portfolio/ProjectCard.jsx
@@ -425,7 +425,6 @@ const ProjectCard = ({ project, index = 0 }) => {
                 </motion.svg>
               </Link>
             </motion.div>
-            </motion.div>
           </div>
         </div>
 
@@ -440,6 +439,7 @@ const ProjectCard = ({ project, index = 0 }) => {
             transition={{ duration: 0.3 }}
           />
         </div>
+      </motion.div>
       </motion.div>
     </motion.div>
   );
